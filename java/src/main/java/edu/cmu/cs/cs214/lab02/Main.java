77d44533bebe411237f1c4421dc05ec49f215ef7
Skylar Gao <jiatongg@andrew.cmu.edu>
2023-09-06 18:21:42 -0400

    encapsulated and decoupled for Java version

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(2.0);
+        Square square = new Square(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rendererRec = new Renderer(rectangle);
+        Renderer rendererCir = new Renderer(circle);
+        Renderer rendererSq = new Renderer(square);
 
-        renderer.draw();
+        rendererRec.draw();
+        rendererCir.draw();
+        rendererSq.draw();
     }
 }
