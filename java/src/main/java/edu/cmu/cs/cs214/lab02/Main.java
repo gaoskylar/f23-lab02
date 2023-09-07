9d7857de5e2c8690f2cb07bb2ee36814506644d0
Skylar Gao <jiatongg@andrew.cmu.edu>
2023-09-06 20:14:12 -0400

    adjustment to adhere to interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -3,12 +3,13 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Square;
 import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-        Circle circle = new Circle(2.0);
-        Square square = new Square(5);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(3.0);
+        Shape square = new Square(5);
 
         Renderer rendererRec = new Renderer(rectangle);
         Renderer rendererCir = new Renderer(circle);
